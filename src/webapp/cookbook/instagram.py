da8b6fe807e1ef92629e78292b7e898be5a2382f
Dennis <dhilhorst2000@gmail.com>
2024-06-28 16:50:23 +0200

    persist instagrapi session

--- src/webapp/cookbook/instagram.py
+++ src/webapp/cookbook/instagram.py
@@ -1,4 +1,5 @@
 import instagrapi
+import instagrapi.exceptions
 import tempfile
 import aiohttp
 import aiofiles
@@ -10,16 +11,75 @@ from .utils import InstagramError, get_headers
 
 
 _CLIENT = instagrapi.Client()
+_PERSIST_DIR = os.getenv("PERSIST_DIR")
 
-def _get_client() -> instagrapi.Client:
-    """ Get (logged in) Instagram client """
-    _CLIENT.login(
+
+def _get_settings_file() -> Path:
+    if _PERSIST_DIR is not None:
+        if not os.path.exists(_PERSIST_DIR):
+            os.makedirs(_PERSIST_DIR, exist_ok=True)
+        return Path(_PERSIST_DIR) / "instagram-session.json"
+    return "instagram-session.json"
+
+
+def _login(dump_settings=False) -> bool:
+    """ Do login with global client """
+    logged_in = _CLIENT.login(
         os.environ["INSTAGRAM_USER"],
         os.environ["INSTAGRAM_PASS"],
         relogin=False
     )
-    return _CLIENT
+    if logged_in and dump_settings:
+        _CLIENT.dump_settings(_get_settings_file())
 
+    return logged_in
+
+
+
+def _get_client() -> instagrapi.Client:
+    """ Get (logged in) Instagram client """
+    settings_file = _get_settings_file()
+    if not os.path.exists(settings_file):
+        print("No instagram session found, logging in with username / password")
+        _login(dump_settings=True)
+        return _CLIENT
+
+    print("Loading instagram session")
+    session = _CLIENT.load_settings(settings_file)
+
+    if session:
+        try:
+            print("Logging in from session")
+            _CLIENT.set_settings(session)
+            _login()
+
+            # check if session is valid
+            try:
+                _CLIENT.get_timeline_feed()
+                return _CLIENT
+            except instagrapi.exceptions.LoginRequired:
+                print("Session is invalid, need to login via username and password")
+
+            old_session = _CLIENT.get_settings()
+
+            # use the same device uuids across logins
+            _CLIENT.set_settings({})
+            _CLIENT.set_uuids(old_session["uuids"])
+
+            if _login(dump_settings=True):
+                # another session test
+                _CLIENT.get_timeline_feed()
+                return _CLIENT
+                
+        except Exception as e:
+            print(f"Couldn't login user using session information: {e}")
+
+    try:
+        print("Attempting to login via username and password.")
+        if _login(dump_settings=True):
+            return _CLIENT
+    except Exception as e:
+        raise InstagramError(f"Couldn't login user with either password or session: {e}")
 
 
 def get_instagram_recipe(url):
