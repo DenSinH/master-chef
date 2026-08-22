1fd600cf92d5f608c116ca5fcc7fe4525369a380
DenSinH <git@dennishilhorst.nl>
2026-08-22 15:08:15 +0200

    remove unused js and make carousel not trip when visibility changes

--- src/webapp/static/messages.js
+++ src/webapp/static/messages.js
@@ -2,13 +2,26 @@ $(document).ready(function() {
     var carouselInner = $('.carousel-inner');
 
     function slide() {
+        if (document.hidden) return;
+
         var firstMessage = carouselInner.children('.message').first();
         var messageWidth = firstMessage.outerWidth(true);
-        carouselInner.animate({ left: -messageWidth }, 500, 'linear', function() {
-            $(this).append(firstMessage);
-            $(this).css('left', 0);
-        });
+
+        carouselInner.stop(true).animate(
+            { left: -messageWidth },
+            500,
+            'linear',
+            function() {
+                $(this).append(firstMessage).css('left', 0);
+            }
+        );
     }
 
     setInterval(slide, 5000);
+
+    document.addEventListener('visibilitychange', function() {
+        if (!document.hidden) {
+            carouselInner.stop(true, true).css('left', 0);
+        }
+    });
 });
